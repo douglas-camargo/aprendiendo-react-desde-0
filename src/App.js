f2a738fc001861dbdefcc4f0b30e954344429f63
douglas camargo <douglascamargo159@gmail.com>
2020-10-10 19:17:15 -0430

    primer commit del curso react desde 0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+// import './App.css';
+import './styles/styles.scss';
+import Curso from './Curso';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App = () => 
+<Fragment>
+
+<div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" alt="Banner" src="https://www.recreoviral.com/wp-content/uploads/2019/03/Fondos-de-pantalla-estilo-Tumblr7.jpg" />
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Practicando react</p>
+        <p> Practicando ando</p>
+        <a href="#" className="button">Botón del banner</a>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
+
+<div className="ed-grid m-grid-3">
+<Curso />
+<Curso />
+<Curso />
+<Curso />
+<Curso />
+<Curso />
+<Curso />
+<Curso />
+<Curso />
+
+</div>
+
+</Fragment>
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// forma para hacer un hola mundo sin jsx
+// const root = document.getElementById("root")
+// const elemento = React.createElement("h1", {className: "saludo"}, "hola mundo")
+// ReactDOM.render(elemento,root)
+
+//forma de hacer hola mundo con jsx
+// const root = document.getElementById("root")
+// ReactDOM.render(<h1 className="saludos">hola mundo</h1>,root)
+
+//forma de hacer hola mundo con componentes jsx
+const root = document.getElementById("root")
+ReactDOM.render(<App />,root)
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
