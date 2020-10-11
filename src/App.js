f6247d30d35e2387e0fcb208ed12cf934dd02753
douglas camargo <douglascamargo159@gmail.com>
2020-10-11 15:04:52 -0430

    clase de componentes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,29 @@ import React, {Fragment} from 'react';
 import './styles/styles.scss';
 import Curso from './Curso';
 
+const cursos = [
+  {
+    "titulo": "React desde 0",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/big/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+    "price": 30,
+    "profesor": "Douglas camargo"
+  },
 
+  {
+    "titulo": "HTML desde 0",
+    "image": "https://edteam-media.s3.amazonaws.com/blogs/smaller/03928ceb-64e7-4336-b18c-6f65a751de6e.jpg",
+    "price": 50,
+    "profesor": "Klyswer Oliva"
+  },
+
+  {
+    "titulo": "CCS desde 0",
+    "image": "https://edteam-media.s3.amazonaws.com/blogs/smaller/d388fad6-edb6-46e9-b0fe-e0d31851f3c5.jpg",
+    "price": 40,
+    "profesor": "Alvaro felipe"
+  }
+
+]
 
 const App = () => 
 <Fragment>
@@ -21,16 +43,22 @@ const App = () =>
   </div>
 </div>
 
+
 <div className="ed-grid m-grid-3">
-<Curso />
-<Curso />
-<Curso />
-<Curso />
-<Curso />
-<Curso />
-<Curso />
-<Curso />
-<Curso />
+{
+  cursos.map(c => <Curso tittle={c.titulo} image={c.image}  price={c.price}  profesor={c.profesor} />)
+
+}
+
+
+{/* <Curso  */}
+{/* 
+    // tittle="curso de React desde cero compadre"
+    // image= "https://ux.ed.team/img/profesor.jpg"
+    // price=""
+    // profesor="" */}
+{/* /> */}
+
 
 </div>
 
